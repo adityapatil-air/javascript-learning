d963d4b0b8a7e1fea6d33366dcc655a9c1f7d889
Aditya Patil <adityapatil.air@gmail.com>
2024-02-24 14:26:38 +0530

    adding test.js file

---
+++ test.js
@@ -0,0 +1,19 @@
+const name="nanna";
+const repoCount=1
+console.log(`my name is ${name} and my repoCount is ${repoCount}`);
+console.log(typeof name);
+const gameName = new String('aditya')
+console.log(gameName.toUpperCase());
+console.log(gameName.length);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('d'));
+const newwString =gameName.substring(0,4 );
+console.log(newwString);
+// const trimString ="   aditya   ";
+// console.log(trimString.trim());
+const test="ad  ity  a     "
+console.log(test.trim());
+const url = ("https://www.instagram.com/")
+console.log(url.includes('inst'));
+const der = ("nanna-aditya-patil")
+console.log(der.split('-'));
