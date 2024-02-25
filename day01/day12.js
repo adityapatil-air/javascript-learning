9c831548b88f6da91365da0ff023eeb98d77ba3d
Aditya Patil <adityapatil.air@gmail.com>
2024-02-25 11:53:14 +0530

    add day 13

--- day01/day12.js
+++ day01/day12.js
@@ -1,6 +1,13 @@
 const score= 400;
-console.log(score);
+// console.log(score);
 const balance=new Number(100)
-console.log(balance);
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+const hundereds=1000000
+console.log(hundereds.toLocaleString('en-IN'));
+// console.log(Math.min(4,5,6,7));
+// console.log(Math.max(4,5,6,7));
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random ()*(max-min+1))+min);
