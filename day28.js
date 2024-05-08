03acbdae7908dac632d62fa84706061fcd83eb35
Aditya Patil <adityapatil.air@gmail.com>
2024-05-08 20:32:34 +0530

    for in for off

---
+++ day28.js
@@ -0,0 +1,18 @@
+let index=0;
+
+while (index<=10) {
+    console.log(`value of index${index}`);
+    index=index+2
+}
+let myArr=['don','rh','chamcha']
+let arr=0
+while (arr<myArr.length) {
+    console.log(`hero is ${myArr[arr]}`);
+    arr=arr+1
+}
+
+let age=0
+do {
+    console.log(`age is ${age}`);
+    age++
+} while (age<=18);
